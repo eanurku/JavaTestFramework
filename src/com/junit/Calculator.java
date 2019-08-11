5343b184d3b7dc875534909593adb53bba6c1bee
Anurag Kumar <anurags62@yahoo.com>
2019-08-11 23:02:14 +0530

    starting project setup

---
+++ src/com/junit/Calculator.java
@@ -0,0 +1,19 @@
+package com.junit;
+
+ class Calculator {
+
+    public int add(int a,int b){
+        return a+b;
+    }
+
+    public int multiply(int a,int b){
+        return a*b;
+    }
+}
+
+class CalculatorTest{
+
+
+
+     
+}
